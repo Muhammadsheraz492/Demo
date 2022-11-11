783332f80e94b621458ba37990cf1e1b0d2477e5
Muhammad Sheraz <90733116+Muhammadsheraz492@users.noreply.github.com>
2022-11-11 18:35:07 +0500

    Understand Numcolum

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text,StyleSheet, FlatList, StatusBar, Dimensions } from 'react-native'
+import React from 'react'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+let width=Dimensions.get('screen').width
+const numColumns=4
+
+const App = () => {
+  const datalist=[{key:'1'},{key:'2'},{key:'3'},{key:'4'},{key:'5'},]
+  let {container,ItemStyle,fontstyle}=style
+  const Render=({item,index})=>{
+   return(
+    <View  style={ItemStyle}>
+      <Text  style={fontstyle}>{item.key}</Text>
     </View>
-  );
+   )
+  }
+  return (
+    <View  style={container}>
+      <StatusBar />
+      <FlatList 
+      data={datalist}
+      renderItem={Render}
+      keyExtractor={(item,index)=>index.toString()}
+      numColumns={numColumns}
+      
+      
+      />
+    </View>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+export default App
+
+const style=StyleSheet.create({
+  container:{
+    flex:1,
+   
   },
-});
+  ItemStyle:{
+    backgroundColor:"#3232ff",
+    alignItems:"center",
+    justifyContent:"center",
+    height:100,
+    flex:1,
+    margin:1,
+    height:width/numColumns
+
+  }
+  ,
+  fontstyle:{
+    fontSize:30,
+    color:"#fff"
+  }
+})
